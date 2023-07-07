25553d8769602425fb80f5cd17ebc5d6fb17b47d
dxthanh2002 <109643053+dxthanh2002@users.noreply.github.com>
2023-07-07 21:55:38 +0700

    a

--- Wine/index.php
+++ Wine/index.php
@@ -19,228 +19,6 @@
     <!-- header section for mobile size  -->
 
     <div class="off_canvas_overlay"></div>
-
-    <div class="offcanvas_menu offcanvas_two">
-        <div class="canvas_open">
-            <a href="javascript:void(0)">
-                <i class="fa fa-bars"></i>
-            </a>
-        </div>
-
-        <div class="offcanvas_menu_wrapper">
-            <div class="canvas_close">
-                <a href="javascript:void(0)">
-                    <i class="fa fa-times"></i>
-                </a>
-            </div>
-            <div class="header_account">
-                <ul>
-                    <li class="language">
-                        <a href="#">
-                            <img src="images/language.png" alt=""> EN <i class="fa fa-angle-down"></i>
-                        </a>
-                        <ul class="dropdown_language">
-                            <li><a href="#">English</a></li>
-                            <li><a href="#">Germany</a></li>
-                            <li><a href="#">Japanese</a></li>
-                        </ul>
-                    </li>
-                    <li class="currency">
-                        <a href="#">
-                            INR<i class="fa fa-angle-down"></i>
-                        </a>
-                        <ul class="dropdown_currency">
-                            <li><a href="#">EUR - EURO</a></li>
-                            <li><a href="#">GBP - British Pound</a></li>
-                            <li><a href="#">USD - US Dollar</a></li>
-                        </ul>
-                    </li>
-                    <li class="top_links">
-                        <a href="#">
-                            My Account <i class="fa fa-angle-down"></i>
-                        </a>
-                        <ul class="dropdown_links">
-                            <li><a href="#">Checkout</a></li>
-                            <li><a href="#">My Account</a></li>
-                            <li><a href="#">Shopping Cart</a></li>
-                            <li><a href="#">Wishlist</a></li>
-                        </ul>
-                    </li>
-
-                </ul>
-            </div>
-
-            <div class="header_right_info">
-                <ul>
-                    <li class="search_box">
-                        <a href="javascript:void(0)">
-                            <i class="fa fa-search"></i>
-                        </a>
-                        <div class="search_widget">
-                            <form action="#">
-                                <input type="text" placeholder="Search Your Wine...">
-                                <button type="submit">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </form>
-                        </div>
-                    </li>
-                    <li class="header_wishlist">
-                        <a href="#">
-                            <i class="fa fa-heart"></i>
-                            <span class="item_count">4</span>
-                        </a>
-                    </li>
-                    <li class="mini_cart_wrapper">
-                        <a href="javascript:void(0)">
-                            <i class="fa fa-shopping-cart"></i>
-                            <span class="item_count">2</span>
-                        </a>
-                        <div class="mini_cart mini_cart2">
-                            <div class="cart_gallery">
-                                <div class="cart_item">
-                                    <div class="cart_img">
-                                        <a href="#">
-                                            <img src="images/cart1.png" alt="">
-                                        </a>
-                                    </div>
-                                    <div class="cart_info">
-                                        <a href="#">
-                                            Quinta do Crasto
-                                        </a>
-                                        <p><span>Rs. 26669</span> X 1</p>
-                                    </div>
-                                    <div class="cart_remove">
-                                        <a href="#"><i class="fa fa-times"></i></a>
-                                    </div>
-                                </div>
-                                <div class="cart_item">
-                                    <div class="cart_img">
-                                        <a href="#">
-                                            <img src="images/cart2.png" alt="">
-                                        </a>
-                                    </div>
-                                    <div class="cart_info">
-                                        <a href="#">
-                                            Domaine Jacques Prieur
-                                        </a>
-                                        <p><span>Rs. 55961</span> X 1</p>
-                                    </div>
-                                    <div class="cart_remove">
-                                        <a href="#"><i class="fa fa-times"></i></a>
-                                    </div>
-                                </div>
-
-                            </div>
-
-                            <div class="mini_cart_table">
-                                <div class="cart_table_border">
-                                    <div class="cart_total">
-                                        <span>Sub Total :</span>
-                                        <span class="price">Rs. 82630</span>
-
-                                    </div>
-                                    <div class="cart_total mt-10">
-                                        <span>Total :</span>
-                                        <span class="price">Rs. 82630</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="mini_cart_footer">
-                                <div class="cart_button">
-                                    <a href="#">View Cart</a>
-                                </div>
-                                <div class="cart_button">
-                                    <a href="#">Checkout</a>
-                                </div>
-
-                            </div>
-                        </div>
-
-
-
-                    </li>
-                </ul>
-            </div>
-
-            <div id="menu" class="text-left">
-                <ul class="offcanvas_main_menu">
-                    <li class="menu-item-has-children active">
-                        <a href="#">Home</a>
-                    </li>
-                    <li class="menu-item-has-children">
-                        <a href="#">Wines</a>
-                        <ul class="sub-menu">
-                            <li class="menu-item-has-children">
-                                <a href="#">Red Wines</a>
-                                <ul class="sub-menu">
-                                    <li><a href="#">Pinot Noir</a></li>
-                                    <li><a href="#">Cabernet Sauvignon</a></li>
-                                    <li><a href="#">Syrah</a></li>
-                                    <li><a href="#">Rhone Blends</a></li>
-                                    <li><a href="#">Merlot</a></li>
-                                    <li><a href="#">Gamay</a></li>
-                                </ul>
-                            </li>
-                            <li class="menu-item-has-children">
-                                <a href="#">White Wines</a>
-                                <ul class="sub-menu">
-                                    <li><a href="#">Chardonnay</a></li>
-                                    <li><a href="#">Riesling</a></li>
-                                    <li><a href="#">Pinot Gris</a></li>
-                                    <li><a href="#">Viogneir</a></li>
-                                </ul>
-                            </li>
-                            <li class="menu-item-has-children">
-                                <a href="#">Others</a>
-                                <ul class="sub-menu">
-                                    <li><a href="#"><b>Champagne & Sparkling</b></a></li>
-                                    <li><a href="#"><b>Rose Wine</b></a></li>
-                                    <li><a href="#"><b>Glassware & Accessories</b></a></li>
-                                    <li><a href="#"><b>Wine Tasting Sets</b></a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="menu-item-has-children"><a href="#">Gifts</a></li>
-                    <li class="menu-item-has-children"><a href="#">About Us</a></li>
-                    <li class="menu-item-has-children">
-                        <a href="#">Region</a>
-                        <ul class="sub-menu">
-                            <li><a href="#">India</a></li>
-                            <li><a href="#">USA</a></li>
-                            <li><a href="#">Australia</a></li>
-                        </ul>
-                    </li>
-                    <li class="menu-item-has-children">
-                        <a href="#">User</a>
-                        <ul class="sub-menu">
-                            <li><a href="#">Log In</a></li>
-                            <li><a href="#">Sign Up</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="offcanvas_footer">
-                <span><a href="#"><i class="fa fa-envelope"></i> wine@gmail.com</a></span>
-                <ul>
-                    <li class="facebook">
-                        <a href="#">
-                            <i class="fab fa-facebook"></i>
-                        </a>
-                    </li>
-                    <li class="twitter"><a href="#"><i class="fab fa-twitter"></i></a></li>
-                    <li class="pinterest"><a href="#"><i class="fab fa-pinterest"></i></a></li>
-                    <li class="linkedin"><a href="#"><i class="fab fa-linkedin"></i></a></li>
-                    <li class="google-plus"><a href="#"><i class="fab fa-google-plus"></i></a></li>
-
-                </ul>
-            </div>
-
-        </div>
-    </div>
-
     <header>
         <div class="main_header header_transparent header-mobile-m">
             <div class="header_container sticky-header">
@@ -248,7 +26,7 @@
                     <div class="row align-items-center">
                         <div class="col-lg-2" style="display: inline-block;">
                             <div class="logo">
-                                <a href="index.html"> <img src="images/logo.png" alt=""></a>
+                                <a href="index.php"> <img src="images/logo.png" alt=""></a>
                             </div>
                         </div>
                         <div class="col-lg-6" style="display: inline-block;">
@@ -256,7 +34,7 @@
                                 <nav>
                                     <ul>
                                         <li>
-                                            <a href="index.html" class="active">Home</a>
+                                            <a href="index.php" class="active">Home</a>
                                         </li>
                                         <li class="mega_items">
                                             <a href="#">Wines <i class="fa fa-angle-down"></i></a>
@@ -294,8 +72,7 @@
                                                 </ul>
                                             </div>
                                         </li>
-                                        <li><a href="#">Gifts</a></li>
-                                        <li><a href="#">About Us</a></li>
+                                        <li><a href="./about_us.php">About Us</a></li>
                                         <li>
                                             <a href="">Region <i class="fa fa-angle-down"></i></a>
                                             <ul class="sub_menu pages">
@@ -307,7 +84,7 @@
                                         <li>
                                             <a href="">User <i class="fa fa-angle-down"></i></a>
                                             <ul class="sub_menu pages">
-                                                <li><a href="#">Log In</a></li>
+                                                <li><a href="./login.php">Log In</a></li>
                                                 <li><a href="#">Sign Up</a></li>
                                             </ul>
                                         </li>
@@ -1307,7 +1084,7 @@
                     </div>
                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                         <div class="copyright">
-                            <p>Wine Shop &copy; 2021. Made with <i class="fa fa-heart"></i>by Techie Coder</p>
+                            <p>Wine Shop &copy; 2021. Made with <i class="fa fa-heart"></i>by Techie Coderrr</p>
                         </div>
                     </div>
                 </div>
