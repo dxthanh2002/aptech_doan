8fb433da2b4581a4c621715199cda73641daedd6
dxthanh2002 <109643053+dxthanh2002@users.noreply.github.com>
2023-07-17 11:06:51 +0700

    danhsach

--- DemoProject_Sanpham/ViewsHome/v_DSSanpham.php
+++ DemoProject_Sanpham/ViewsHome/v_DSSanpham.php
@@ -1,44 +1,48 @@
-        <div id="content_center">
+        <div class="container">
         	<h1>
-            <?php
-			if($act=="timkiem")
-				echo "KẾT QUẢ TÌM KIẾM";
-			else
-				echo "DANH SÁCH SẢN PHẨM";
-			?>
-            </h1>
-            <?php
-			if($sanpham->data==NULL)
+        		<?php
+				if ($act == "timkiem")
+					echo "KẾT QUẢ TÌM KIẾM";
+				else
+					echo "DANH SÁCH SẢN PHẨM";
+				?>
+        	</h1>
+        	<?php
+			if ($sanpham->data == NULL)
 				echo "<H2>KHÔNG TÌM THẤY SẢN PHẨM</H2>";
 			else
-				if($act=="timkiem")
-					echo "<H2>CÓ ". $soketqua . " SẢN PHẨM</H2>";
-				else if($tennhom!="")
-					echo "<H2> Loại sách: " . $tennhom . "</H2>";
-					
+				if ($act == "timkiem")
+				echo "<H2>CÓ " . $soketqua . " SẢN PHẨM</H2>";
+			else if ($tennhom != "")
+				echo "<H2> Loại sách: " . $tennhom . "</H2>";
+
 			?>
-            <div id="content_sp">
-              <?php
-              	$rows = $sanpham->data;
-				foreach($rows as $row)
-				{
+        	<div class="row d-flex justify-content-around mx-auto mt-3">
+        		<?php
+				$rows = $sanpham->data;
+				foreach ($rows as $row) {
 					$hinhanh = $row["images"];
-					if($hinhanh=="")
+					if ($hinhanh == "")
 						$hinhanh = "no-Image.png";
-					$trangthai="";
-					if($row["status"]==1)
+					$trangthai = "";
+					if ($row["status"] == 1)
 						$trangthai = "có";
 					else
 						$trangthai = "không";
 				?>
-              <div class="sanpham" style="width:200px; height:350px;">
-                <p><a href="?module=chitietsanpham&manhom=<?=$row["cat_id"]?>&masp=<?=$row["id"]?>"><?=$row["title"]?></a></p><!-- thẻ <a href>...</a> để tạo liên kết tới sp1.htm-->
-                <p><a href="?module=chitietsanpham&manhom=<?=$row["cat_id"]?>&masp=<?=$row["id"]?>"><img src="Hinhanh/Sanpham/<?=$hinhanh?>"></a></p> <!--thẻ p này dùng để cố định chiều cao dòng 2, và để ảnh căn giữa-->
-                <p>Giá: <?=number_format($row["price"])?> VNĐ</p>
-                <p><i class="fa fa-shopping-cart"></i><a href="?module=cart&act=add&masp=<?=$row["id"]?>"> Mua hàng</a></p>
-              </div>
-				<?php
+
+        			<div class=" card text-center mt-5" style="width: 18rem;">
+        				<a class="card-link" href="?module=chitietsanpham&manhom=<?= $row["cat_id"] ?>&masp=<?= $row["id"] ?>"><img src="Hinhanh/Sanpham/<?= $hinhanh ?>" class="card-img-top"></a>
+        				<div class="card-body">
+        					<h5 class="card-title"><a href="?module=chitietsanpham&manhom=<?= $row["cat_id"] ?>&masp=<?= $row["id"] ?>" class="card-link"><?= $row["title"] ?></a></h5><!-- thẻ <a href>...</a> để tạo liên kết tới sp1.htm-->
+        					<!--thẻ p này dùng để cố định chiều cao dòng 2, và để ảnh căn giữa-->
+        					<p class="card-text">Giá: <?= number_format($row["price"]) ?> VNĐ</p>
+        					<i class="fa fa-shopping-cart"></i><a href="?module=cart&act=add&masp=<?= $row["id"] ?>" class="btn btn-primary"> Mua hàng</a>
+        				</div>
+        			</div>
+
+        		<?php
 				}
 				?>
-            </div>
-       </div>
+        	</div>
+        </div>
