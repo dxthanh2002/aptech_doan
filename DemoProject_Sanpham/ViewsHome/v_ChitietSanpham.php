32775d3c596db6d597b9bc895106a55e10a19868
dxthanh2002 <109643053+dxthanh2002@users.noreply.github.com>
2023-07-20 19:42:45 +0700

    a

--- DemoProject_Sanpham/ViewsHome/v_ChitietSanpham.php
+++ DemoProject_Sanpham/ViewsHome/v_ChitietSanpham.php
@@ -1,6 +1,7 @@
         <div class="row  d-flex justify-content-evenly">
           <div class="col-1 col-md-8">
             <h1>Thông tin về rượu</h1>
+            <hr>
             <?php
             if ($sanpham->data == NULL)
               echo "<H2>KHÔNG TÌM THẤY SẢN PHẨM</H2>";
@@ -11,31 +12,31 @@
                 $hinhanh = "no-Image.png";
             ?>
               <div id="content_sp">
-                <div class="row">
-                <div class=" card" style="width: 18rem;">
-                  <p><a style="text-decoration: none;" href="#"><?= $row["title"]; ?></a></p><!-- thẻ <a href>...</a> để tạo liên kết tới sp1.htm-->
-                  <p><a href="#"><img src="Hinhanh/Sanpham/<?= $hinhanh ?>" class="card-img-top"></a></p> <!--thẻ p này dùng để cố định chiều cao dòng 2, và để ảnh căn giữa-->
-                </div>
-                <div class="tomtatsanpham card" style="width:580px; height:350px;padding:5px;">
-                  <h2> Thông tin sản phẩm</h2>
-                  <div class="noidung_tomtat" style="margin:10px; padding:10px;">
-                    <ul class="list-group list-group-flush">
-                      <li class="list-group-item">Thương hiệu: <?= $row["author"] ?></li>
-                      <li class="list-group-item"> Xuất xứ : <?= $row["summary"] ?> </li>
-                      <li class="list-group-item"> Nồng Độ: <?= $row["concentration"] ?> % </li>
-                      <li class="list-group-item"> Dung tích: <?= $row["capacity"] ?> ml </li>
-                    </ul>
+                <div class="row justify-content-start">
+                  <div class="col-1 col-sm-2 col-md-3 mx-3 card " style="width: 18rem;">
+                    <p><a style="text-decoration: none;" href="#"><?= $row["title"]; ?></a></p><!-- thẻ <a href>...</a> để tạo liên kết tới sp1.htm-->
+                    <p><a href="#"><img src="Hinhanh/Sanpham/<?= $hinhanh ?>" class="card-img-top"></a></p> <!--thẻ p này dùng để cố định chiều cao dòng 2, và để ảnh căn giữa-->
                   </div>
-                  <h3>Giá: <?= number_format($row["price"]) ?> VNĐ</h3>
-                  <h3><i class="fa fa-shopping-cart"></i><a href="?module=cart&act=add&masp=<?= $row["id"] ?>"> Mua hàng</a></h3>
+                  <div class=" tomtatsanpham card col-1 col-sm-4 col-md-6 " >
+                    <h2> Thông tin sản phẩm</h2>
+                    <div class="noidung_tomtat" style="margin:10px; padding:10px;">
+                      <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Thương hiệu: <?= $row["author"] ?></li>
+                        <li class="list-group-item"> Xuất xứ : <?= $row["summary"] ?> </li>
+                        <li class="list-group-item"> Nồng Độ: <?= $row["concentration"] ?> % </li>
+                        <li class="list-group-item"> Dung tích: <?= $row["capacity"] ?> ml </li>
+                      </ul>
+                    </div>
+                    <h3>Giá: <?= number_format($row["price"]) ?> VNĐ</h3>
+                    <h3><i class="fa fa-shopping-cart"></i><a href="?module=cart&act=add&masp=<?= $row["id"] ?>"> Mua hàng</a></h3>
+                  </div>
                 </div>
-                </div>
-                <div class="chitietsanpham row">
+                <div class="chitietsanpham col-9">
                   <div class="card">
-                  <h2 class="card-title">CHI TIẾT SẢN PHẨM</h2>
-                  <p class="card-text"> <?= $row["content"] ?> </p>
+                    <h2 class="card-title">CHI TIẾT SẢN PHẨM</h2>
+                    <p class="card-text"> <?= $row["content"] ?> </p>
                   </div>
-                  
+
                 </div>
 
               </div>
