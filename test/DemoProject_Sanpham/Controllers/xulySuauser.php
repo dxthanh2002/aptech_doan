b7623ff3f6d5ce32b41e96f0e70187282f12afc3
dxthanh2002 <109643053+dxthanh2002@users.noreply.github.com>
2023-07-29 10:30:36 +0700

    s

--- test/DemoProject_Sanpham/Controllers/xulySuauser.php
+++ test/DemoProject_Sanpham/Controllers/xulySuauser.php
@@ -4,17 +4,12 @@ $id = $_REQUEST["id"];
 $user = $_REQUEST["t1"];
 $pass = $_REQUEST["t2"];
 $fullname = $_REQUEST["t3"];
-
-	
 $tel = $_REQUEST["t4"];
-$adress = $_REQUEST["t4a"];
-$email = $_REQUEST["t4b"];
- 
+$adress = $_REQUEST["t5"];
+$email = $_REQUEST["t6"];
 $status =1;
 if(isset($_REQUEST["rTrangthai"]))
 	$status = $_REQUEST["rTrangthai"];
- 
-
 $ketqua = $sanpham->Suafulluser($id,$user,$pass, $fullname, $tel,$adress,$email,$status);
 if($ketqua==FALSE)
 	$thongbao="Lỗi sửa dữ liệu";
