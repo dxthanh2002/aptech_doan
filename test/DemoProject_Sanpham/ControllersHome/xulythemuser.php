3207a316eca6b2e8634dff153b196b53013ecca5
dxthanh2002 <109643053+dxthanh2002@users.noreply.github.com>
2023-08-22 17:30:42 +0700

    sualancuoi

--- test/DemoProject_Sanpham/ControllersHome/xulythemuser.php
+++ test/DemoProject_Sanpham/ControllersHome/xulythemuser.php
@@ -2,16 +2,16 @@
 
 require_once("Dungchung/tienich.php");
 $username = $_REQUEST["t1"];
-$pass = md5($_REQUEST["t2"]);
+$pass = $_REQUEST["t2"];
 $name = $_REQUEST["t3"];
 $tel = $_REQUEST["t4"];
 $adress = $_REQUEST["t5"];
 $mail = $_REQUEST["t6"];
-$trangthai = 0;
+$trangthai = 1;
 if(isset($_REQUEST["rTrangthai"]))
 	$trangthai = $_REQUEST["rTrangthai"];
 	$token = md5($_POST['t6']).rand(10,9999);
-$ketqua = $user->Themuser($username,$pass,$name,  $tel, $adress,$mail,$trangthai);
+$ketqua = $user->Themuser($username,$pass,$name,$tel,$adress,$mail,$trangthai);
 if($ketqua==FALSE)
 	$thongbao="Lỗi thêm dữ liệu";
 else
